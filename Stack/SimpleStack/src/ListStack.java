b3bda4edbb10bf6d0eae07ac0fa97aa87c28bb62
kurakin.as <aleksandr.kurakin@domru.ru>
2019-12-10 14:11:51 +0500

    Added list based stack

---
+++ Stack/SimpleStack/src/ListStack.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStack<T> {
+    private List<T> arr;
+    private int top;
+    private int capacity;
+
+    // Constructor to initialize stack
+    ListStack(int size) {
+        arr = new ArrayList<>();
+        capacity = size;
+        top = -1;
+    }
+
+    // Utility function to add an element x in the stack
+    public void push(T x) throws Exception {
+        if (isFull()) {
+            throw new Exception("Stack overflow!");
+        }
+
+        System.out.println("Inserting " + String.valueOf(x));
+        arr.add(x);
+        top++;
+    }
+
+    // Utility function to pop top element from the stack
+    public T pop() throws Exception {
+        // check for stack underflow
+        if (isEmpty()) {
+            throw new Exception("Stack is empty. Nothing to pop.");
+        }
+
+        System.out.println("Removing " + String.valueOf(peek()));
+
+        // decrease stack size by 1 and (optionally) return the popped element
+        top--;
+        return arr.get(top+1);
+    }
+
+    // Utility function to return top element in a stack
+    public T peek() throws Exception {
+        if (!isEmpty())
+            return arr.get(top);
+        else
+            throw new Exception("Stack is empty. Nothing to peek.");
+    }
+
+    // Utility function to return the size of the stack
+    public int size() {
+        return top + 1;
+    }
+
+    // Utility function to check if the stack is empty or not
+    public Boolean isEmpty() {
+        return top == -1;	// or return size() == 0;
+    }
+
+    // Utility function to check if the stack is full or not
+    public Boolean isFull() {
+        return top == capacity - 1;	// or return size() == capacity;
+    }
+}
--- Stack/SimpleStack/src/Main.java
+++ Stack/SimpleStack/src/Main.java
@@ -24,5 +24,30 @@ public class Main {
             System.out.println("Stack is empty");
         else
             System.out.println("Stack is not empty");
+
+        ListStack listStack = new ListStack(5);
+
+        try {
+            listStack.push(1);		// Inserting 1 in the stack
+            listStack.push(2);		// Inserting 2 in the stack
+
+            listStack.pop();		// removing the top 2
+            listStack.pop();		// removing the top 1
+
+            listStack.push(3);		// Inserting 3 in the stack
+
+            System.out.println("Top element is: " + listStack.peek());
+            System.out.println("Stack size is " + listStack.size());
+
+            listStack.pop();		// removing the top 3
+        } catch (Exception e) {
+            System.out.println("Exception occured: " + e.getMessage());
+        }
+
+        // check if stack is empty
+        if (listStack.isEmpty())
+            System.out.println("Stack is empty");
+        else
+            System.out.println("Stack is not empty");
     }
 }
